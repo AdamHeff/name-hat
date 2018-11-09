e69aeb8378d958a85463a343825bc96994ff65de
Adam Heffelbower <adam.heffelbower@greatclips.com>
2018-11-09 16:46:38 -0600

    Got the basics of the calculate function mapped out.

--- calculate.js
+++ calculate.js
@@ -1,3 +1,16 @@
 function calculate(families) {
-    return "calculate: " + families;
+    // Copy all the names into a straight list
+    var names;
+    families.forEach(fam => {
+        fam.forEach(person => {
+            names.push(person);
+        })
+    });
+
+    // Go through the list, generate a random match.
+    Math.floor(Math.random() * names.length);
+
+    // Test the match on 3 criteria. If it fails, try agian.
+
+    // If there is someone extra left over, try the whole thing again.
 }
