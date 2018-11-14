380b812a1439f2c21a1469c7eab6f268201e5308
Adam Heffelbower <adam.heffelbower@greatclips.com>
2018-11-14 11:56:48 -0600

    Re-wrote the algo to use objects.

--- calculate.js
+++ calculate.js
@@ -1,114 +1,92 @@
 function calculate(families) {
 
     // Copy all the names into a straight list
-    let names = new Array();
+    let personInfos = new Array();
     families.forEach(fam => {
         fam.forEach(person => {
-            names.push(person);
+            let personInfo = new Object();
+            personInfo.name = person;
+            personInfo.taken = false;
+            personInfo.fam = fam;
+            personInfo.receiverName = null;
+
+            personInfos.push(personInfo);
         })
     });
 
     // Go through the list, generate a random match.
     let totalFailure = false;
-    let matches = new Array();
-    families.forEach(fam => {
-        fam.forEach(giver => {    
-            let origIndex = Math.floor(Math.random() * names.length);
-            let currIndex = origIndex;
+    personInfos.forEach(giverInfo => {
+        let receiverInfo = null;
 
-            // Find a match
-            let doneLooping = false;
-            let matchFound = false;
-            while(doneLooping == false) {
-                var receiver = names[currIndex];
-                // Is the person in the same family? (Including self)? Is the person taken?
-                if(inFamily(fam, receiver) || alreadyTaken(matches, receiver)) {
-                    currIndex++;
-                    if(currIndex >= names.length) {
-                        currIndex = 0;
-                    }
-                    if(currIndex == origIndex) {
-                        // We went through the whole list and didn't find a match. Bailout, with no match
-                        doneLooping = true;
-                    }
+        let origIndex = Math.floor(Math.random() * personInfos.length);
+        let currIndex = origIndex;
+
+        // Find a match
+        let doneLooping = false;
+        let matchFound = false;
+        while(doneLooping == false) {
+            receiverInfo = personInfos[currIndex];
+            // Is the person in the same family? (Including self)? Is the person taken?
+            if(inFamily(giverInfo.fam, receiverInfo.name) || receiverInfo.taken) {
+                currIndex++; // todo: find a better way to increment if possible.
+                if(currIndex >= personInfos.length) {
+                    currIndex = 0;
                 }
-                else {
+                if(currIndex == origIndex) {
+                    // We went through the whole list and didn't find a match. Bailout, with no match
                     doneLooping = true;
-                    matchFound = true;
                 }
             }
-
-            if(!matchFound)
-            {
-                // If we make it here, we need to try a swap as a last ditch effort
-                let notTaken = getNotTaken(matches, names);
-                let matchFriend = null;
-                matches.forEach(match => {
-                    if(matchFriend == null) {
-                        if(!inFamily(getFamily(match.giver, families), notTaken) && !inFamily(getFamily(giver, families), match.receiver)) {
-                            // Perform swap
-                            matchFriend = match;
-                        }
-                    }
-                });
-
-                if(matchFriend != null) {
-                    let tempReciever = matchFriend.receiver;
-                    matchFriend.receiver = notTaken;
-                    receiver = tempReciever;
-                } else {
-                    totalFailure = true;
-                }
+            else {
+                doneLooping = true;
+                matchFound = true;
             }
+        }
+        if(matchFound) {
+            giverInfo.receiverName = receiverInfo.name;
+            receiverInfo.taken = true;    
+        } else {
 
-            var match = new Object();
-            match.giver = giver;
-            match.receiver = receiver;
-            matches.push(match);
-        })
+            // If we make it here, we need to try a swap as a last ditch effort (This is a rare case, but is most of
+            // the complexity. It's when the last person draws her own name, or the name of someone in her family.)
+            let notTakenInfo = personInfos.find(x => x.taken == false);
+            let matchFriend = null;
+            personInfos.forEach(person => {
+                if(!inFamily(notTakenInfo.fam, person.name) && !inFamily(giverInfo.fam, person.receiverName)) {
+                    matchFriend = person;
+                }
+            });
+
+            if(matchFriend != null) {
+                let tempRecieverName = matchFriend.receiverName;
+                matchFriend.receiverName = notTakenInfo.name;
+                notTakenInfo.taken = true;
+                giverInfo.receiverName = tempRecieverName;
+            } else {
+                totalFailure = true;
+            }
+        }
     })
 
     var response = new String();
     if(totalFailure) {
         response = "Unable to make a match with this data."
     } else {
-        matches.forEach(match => {
-            response += match.giver + " -> " + match.receiver + "\n";
+        personInfos.forEach(person => {
+            response += person.name + " -> " + person.receiverName + "\n";
         });
     }
     return response;
 }
 
 function inFamily(fam, receiver) {
+    // todo: simplify later
     let returnVal = false;
     fam.forEach(person => {
         if(receiver == person) {
             returnVal = true;
         }
     })
-    return returnVal;
-}
-
-function alreadyTaken(matches, receiver) {
-    let returnVal = false;
-    matches.forEach(match => {
-        if(match.receiver == receiver) {
-            returnVal = true;
-        }
-    })
-    return returnVal;
-}
-
-function getFamily(person, families) {
-    return families.find(fam => fam.indexOf(person) > -1);
-}
-
-function getNotTaken(matches, names) {
-    let returnVal = new String();
-    names.forEach(name => {
-        if(!alreadyTaken(matches, name)) {
-            returnVal = name;
-        }
-    });
     return returnVal;
 }
