e4fcc3b4840ee25ff9562770c5d4dffa622c0bac
Adam Heffelbower <adam.heffelbower@greatclips.com>
2018-12-20 08:14:15 -0600

    Lots of updates as I read through Clean Code by Uncle Bob.

--- js/makematches.js
+++ js/makematches.js
@@ -21,6 +21,11 @@ function makematches(families) {
     return makematchesBody(families, Math.random);
 }
 
+// todo: 1. make this whole thing into a class/function so that I don't have to pass
+// so many parameters to the functions that are called by makematchesBody.
+// todo: 2. continue to breakdown this function until it has fewer than 20 lines.
+// todo: 3. Update my comments to be in line with clean code principles.
+
 // The main body that will output a match if at all possible.
 function makematchesBody(families, randomFunc) {
 
@@ -29,16 +34,15 @@ function makematchesBody(families, randomFunc) {
     initializeGiversAndReceivers(families, givers, receivers);
 
     // Go through the list, generate a random match.
-    var unmatchableList = false;
-    givers.forEach(function (giver) {
+    for(var outer=0; outer<givers.length; outer++) {
+        var giver = givers[outer];
         var idx = Math.floor(randomFunc() * receivers.length);
+
         for(var i=0; i<receivers.length; i++) {
             if (giver.isOkReceiver(receivers[idx])) {
-
                 giver.receiverName = receivers[idx];
                 receivers.splice(idx, 1);
                 break;
-
             } else {
 
                 idx++;
@@ -47,32 +51,18 @@ function makematchesBody(families, randomFunc) {
                 }
                 if (i == receivers.length-1) {
 
-                    // We went through the whole list and didn't find a match. (In normal scenarios, this
-                    // is a very rare case when the last guy draws his own name.)
-                    unmatchableList = true;                    
-                    for(var j=0; j<givers.length; j++) {
-                        if(givers[j].isOkReceiver(receivers[0]) && giver.isOkReceiver(givers[j].receiverName)) {                            
-                            unmatchableList = false;
-                            let tempRecieverName = givers[j].receiverName;
-                            givers[j].receiverName = receivers[0];
-                            giver.receiverName = tempRecieverName;
-                            receivers.splice(0, 1);
-                            break;
-                        }
+                    if(!swapReceiverWithSomeone(giver, givers, receivers)) {
+                        return 'Unable to make a match with this data.'
                     }
                 }
             }
         }
-    });
-
-    var response = new String();
-    if (unmatchableList) {
-        response = "Unable to make a match with this data.";
-    } else {
-        givers.forEach(function (giver) {
-            response += giver.name + " -> " + giver.receiverName + "\n";
-        });
     }
+
+    var response = '';
+    givers.forEach(function (giver) {
+        response += giver.name + " -> " + giver.receiverName + "\n";
+    });
     return response;
 }
 
@@ -84,6 +74,22 @@ function initializeGiversAndReceivers(families, givers, receivers) {
             receivers.push(person);
         });
     });
+}
+
+// We went through the whole list and didn't find a match. (In normal scenarios, this
+// is a very rare case when the last guy draws his own name.)
+function swapReceiverWithSomeone(giver, givers, receivers) {
+    for(var j=0; j<givers.length; j++) {
+        if(givers[j].isOkReceiver(receivers[0]) && giver.isOkReceiver(givers[j].receiverName)) {                            
+            madeASwap = true;
+            let tempRecieverName = givers[j].receiverName;
+            givers[j].receiverName = receivers[0];
+            giver.receiverName = tempRecieverName;
+            receivers.splice(0, 1);
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = makematchesBody;
